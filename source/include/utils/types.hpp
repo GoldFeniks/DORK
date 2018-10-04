a2a8c37f25058c11aae51b5237c49cb11cf24b5d
Tyshchenko Andrey <ggoldenfeniks@gmail.com>
2018-10-04 16:48:19 +1000

    [utils] Add types

---
+++ source/include/utils/types.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <vector>
+#include <array>
+
+namespace ssh {
+
+    namespace types {
+
+        template<typename T>
+        using vector1d_t = std::vector<T>;
+
+        template<typename T>
+        using vector2d_t = std::vector<std::vector<T>>;
+
+        template<typename T, size_t N>
+        using array1d_t = std::array<T, N>;
+
+        template<typename T, size_t N>
+        using array2d_t = std::array<std::array<T, N>, N>;
+
+    }
+
+}
