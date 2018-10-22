67f8d5a4e30b73bbf8a54ae5067602527b9d4708
Tyshchenko Andrey <ggoldenfeniks@gmail.com>
2018-10-22 21:34:55 +1000

    [dense_output] Rework dense_output

--- source/include/runge_kutta/dense_output.hpp
+++ source/include/runge_kutta/dense_output.hpp
@@ -2,59 +2,118 @@
 
 #include <functional>
 #include <cmath>
+#include <type_traits>
 #include "../utils/types.hpp"
 
 namespace ssh {
 
+    template<typename Argument, size_t N = 0, typename Value = Argument, typename Functions = types::vector1d_t<std::function<Value(const Argument&)>>>
     class dense_output {
+        
+        public:
 
-    public:
+            dense_output() = delete;
+            ~dense_output() = default;
 
-        template<typename T, typename Coeffs, typename Vector = types::vector1d_t<T>, size_t N = 0>
-        static auto create(const Coeffs& coefficients, const T& h, const T& y0, const Vector& k) {
-            const auto functions = coefficients.template generate_functions<N>();
-            const auto n = coefficients.height();
-            return [functions, h, y0, k, n](const T x) {
-                auto result = T(0);
-                for (size_t i = 0; i < n; ++i)
-                    result += functions[i](x) * k[i];
-                return (N == 0 ? y0 : 0) + h * result;
-            };
-        }
+            dense_output(const dense_output&) = default;
+            dense_output(dense_output&&) = default;
 
-        template<typename T, typename Coeffs, typename Vector = types::vector1d_t<T>, size_t N = 0>
-        static auto create_bound(const Coeffs& coefficients, const T& h, const T& y0, const Vector& k) {
-            const auto functions = coefficients.template generate_functions<N>();
-            const auto n = coefficients.height();
-            return [functions, h, &y0, &k, n](const T x) {
-                auto result = T(0);
-                for (size_t i = 0; i < n; ++i)
-                    result += functions[i](x) * k[i];
-                return (N == 0 ? y0 : 0) + h * result * std::pow(2, N);
-            };
-        }
+            dense_output(const Functions& functions) : _functions(functions) {}
 
-    };
-
-    class runge_kutta_dense_output {
-
-    public:
-
-        template<typename T, typename RK, typename Coeffs, typename Vector = types::vector1d_t<T>>
-        static Vector solve(RK& rk, const T& h, const T& y0, const size_t n, const Coeffs& coefficients) {
-            types::vector1d_t<T> k(coefficients.height());
-            auto v = y0;
-            Vector result;
-            auto output = dense_output::create_bound<T, Coeffs, Vector>(coefficients, h, v, k);
-            while (rk) {
-                auto v1 = rk(k);
-                for (size_t i = 0; i < n; ++i)
-                    result.push_back(output(T(1) / T(n) * i));
-                v = v1;
+            template<typename Vector = types::vector1d_t<Value>>
+            auto interpolate(const Argument& h, const Vector& k) {
+                return interpolate<Vector>(h, k, k[0]);
             }
-            result.push_back(v);
-            return result;
-        }
+
+            template<typename Vector = types::vector1d_t<Value>>
+            auto interpolate(const Argument& h, const Vector& k, const Value& y) {
+                return [functions=_functions, h, k, y](const Argument& x) {
+                    auto result = Value(0);
+                    for (size_t i = 0; i < k.size(); ++i)
+                        result += functions[i](x) * k[i];
+                    return (N == 0 ? y : Value(0)) + h * result * std::pow(2, N);
+                };
+            }
+
+            template<typename KVector = types::vector2d_t<Value>, typename RVector = typename KVector::value_type>
+            auto interpolate_p(const Argument& h, const KVector& k) {
+                return interpolate_p<KVector, RVector>(h, k, k[0]);
+            }
+
+            template<typename KVector = types::vector2d_t<Value>, typename RVector = types::vector1d_t<Value>>
+            auto interpolate_p(const Argument& h, const KVector& k, const RVector& y) {
+                return [functions=_functions, h, k, y](const Argument& x) {
+                    auto result = std::is_constructible_v<RVector, size_t, Value>() ? RVector(k.size(), Value(0)) : RVector();
+                    for (size_t i = 0; i < k.size(); ++i) {
+                        for (size_t j = 0; j < functions.size(); ++j)
+                            result[i] += functions[j](x) * k[i][j];
+                        result[i] = (N == 0 ? y[i] : Value(0)) + h * result[i] * std::pow(2, N);
+                    }
+                };
+            }
+
+            template<typename KVector = types::vector2d_t<Value>>
+            auto interpolate_bound(const Argument& h, const KVector& k) {
+                return interpolate_bound<KVector>(h, k, k[0]);
+            }
+
+            template<typename KVector = types::vector2d_t<Value>>
+            auto interpolate_bound(const Argument& h, const KVector& k, const Value& y) {
+                return [functions=_functions, &h, &k, &y](const Argument& x) {
+                    auto result = Value(0);
+                    for (size_t i = 0; i < k.size(); ++i)
+                        result += functions[i](x) * k[i];
+                    return (N == 0 ? y : Value(0)) + h * result * std::pow(2, N);
+                };
+            }
+
+            template<typename KVector = types::vector2d_t<Value>, typename RVector = typename KVector::value_type>
+            auto interpolate_bound_p(const Argument& h, const KVector& k) {
+                return interpolate_bound_p<KVector, RVector>(h, k, k[0]);
+            }
+
+            template<typename KVector = types::vector2d_t<Value>, typename RVector = types::vector1d_t<Value>>
+            auto interpolate_bound_p(const Argument& h, const KVector& k, const RVector& y) {
+                return [functions=_functions, &h, &k, &y](const Argument& x) {
+                    auto result = std::is_constructible_v<RVector, size_t, Value>() ? RVector(k.size(), Value(0)) : RVector();
+                    for (size_t i = 0; i < k.size(); ++i) {
+                        for (size_t j = 0; j < functions.size(); ++j)
+                            result[i] += functions[j](x) * k[i][j];
+                        result[i] = (N == 0 ? y[i] : Value(0)) + h * result[i] * std::pow(2, N);
+                    }
+                };
+            }
+
+            template<typename Coeffs>
+            static auto from_coefficients() { 
+                return dense_output(Coeffs().template generate_functions<N>());
+            }
+
+            template<typename Coeffs>
+            static auto from_coefficients(const Coeffs& coefficients) {
+                return dense_output(coefficients.template generate_functions<N>());
+            }
+
+            template<typename RK, typename RVector = types::vector1d_t<Value>>
+            auto solve(RK& rk, const Argument& h, const Value& y0, const size_t n) {
+                types::vector1d_t<Value> k(_functions.size());
+                auto v = y0;
+                RVector result;
+                auto output = interpolate_bound(h, k, v);
+                while (rk) {
+                    result.push_back(v);
+                    auto v1 = rk(k);
+                    for (size_t i = 1; i < n; ++i)
+                        result.push_back(output(Argument(1) / Argument(n) * Argument(i)));
+                    v = v1;
+                }
+                result.push_back(v);
+                return result;
+            }
+
+        private:
+
+            const Functions _functions;
 
     };
 
--- source/main.cpp
+++ source/main.cpp
@@ -9,29 +9,30 @@
 #include <fstream>
 
 int main() {
-    const auto args = ssh::utils::create_mesh_bounds<ssh::types::vector1d_t<double>>(0., 10., 101);
-    std::vector<std::array<double, 1>> params;
-    for (size_t i = 0; i < 11; ++i)
-        params.push_back({ i / 10. * M_PI_2 });
-    std::vector<double> init;
-    for (const auto& it : params)
-        init.push_back(std::sin(it[0]));
-    auto rk = ssh::lazy_runge_kutta_p(args, init, [](double x, double y, double z) { return std::cos(x + z); }, params, ssh::runge_kutta_coefficients<double, 4>());
+    // const auto args = ssh::utils::create_mesh_bounds<ssh::types::vector1d_t<double>>(0., 10., 101);
+    // std::vector<std::array<double, 1>> params;
+    // for (size_t i = 0; i < 11; ++i)
+    //     params.push_back({ i / 10. * M_PI_2 });
+    // std::vector<double> init;
+    // for (const auto& it : params)
+    //     init.push_back(std::sin(it[0]));
+    // auto rk = ssh::lazy_runge_kutta_p(args, init, [](double x, double y, double z) { return std::cos(x + z); }, params, ssh::runge_kutta_coefficients<double, 4>());
+    // std::ofstream out("./source/cmake-build-debug/test.txt");
+    // for (const auto& it : init)
+    //     out << it << ' ';
+    // out << '\n';
+    // while (rk) {
+    //     for (const auto& it : rk()) {
+    //         out << it << ' ';
+    //     }
+    //     out << '\n';
+    // }
+    auto rk = ssh::lazy_runge_kutta_uniform(0., 10., 21, 1., [](double x, double y) { return -std::sin(x); }, ssh::runge_kutta_coefficients<double, 4>());
+    auto output = ssh::dense_output<double>::from_coefficients(ssh::runge_kutta_dense_output_coefficients<double, 3>());
+    auto res = output.solve(rk, 0.5, 1, 5);
     std::ofstream out("./source/cmake-build-debug/test.txt");
-    for (const auto& it : init)
-        out << it << ' ';
-    out << '\n';
-    while (rk) {
-        for (const auto& it : rk()) {
-            out << it << ' ';
-        }
-        out << '\n';
-    }
-    // auto rk = ssh::lazy_runge_kutta_uniform(0., 10., 21, 0., [](double x, double y) { return std::cos(x) + std::sin(x); }, ssh::runge_kutta_coefficients<double, 4>());
-    // auto o = ssh::runge_kutta_dense_output::solve(rk, 0.5, 0., 11, ssh::runge_kutta_dense_output_coefficients<double, 3>());
-    // std::ofstream out("test.txt");
-    // for (const auto it : o)
-        // out << it << ' ';
+    for (const auto it : res)
+        out << it << '\n';
 }
 
 
