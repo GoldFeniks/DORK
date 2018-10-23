4c128a69cca0d4d526a91ab7d6a67f7c0e25d158
Tyshchenko Andrey <ggoldenfeniks@gmail.com>
2018-10-23 18:53:25 +1000

    [runge_kutta] Add bound runge_kutta versions

--- source/include/runge_kutta/runge_kutta.hpp
+++ source/include/runge_kutta/runge_kutta.hpp
@@ -2,7 +2,6 @@
 
 #include "coefficients.hpp"
 #include "../utils/utils.hpp"
-#include <functional>
 #include <tuple>
 #include <type_traits>
 
@@ -10,10 +9,10 @@ namespace ssh {
 
     namespace {
 
-        template<typename Argument, typename Function, typename Value = Argument, typename... Parameters>
+        template<typename Argument, typename Function, bool  StoreRef = false, typename Value = Argument, typename... Parameters>
         struct caller {
 
-            Function function;
+            std::conditional_t<StoreRef, const Function&, const Function> function;
 
             caller(const Function& function) : function(function) {}
 
@@ -33,7 +32,9 @@ namespace ssh {
 
     namespace implementation {
 
-        template<typename Vector, typename Value, typename Function, typename Coeffs, typename KVector = types::vector1d_t<Value>>
+        template<
+            typename Vector, typename Value, typename Function, typename Coeffs,
+            bool StoreRef = false, typename KVector = types::vector1d_t<Value>>
         class lazy_runge_kutta {
 
         public:
@@ -51,7 +52,10 @@ namespace ssh {
 
             lazy_runge_kutta(const Vector& arguments, const Value& initial_value, 
                              const Function& function) :
-                lazy_runge_kutta(arguments, initial_value, function, Coeffs()) {}
+                lazy_runge_kutta(arguments, initial_value, function, Coeffs()) 
+            {
+                static_assert(!StoreRef, "Coeffitients must be provided to store reference");
+            }
 
             Value operator()(KVector& k) {
                 if (!*this)
@@ -87,17 +91,21 @@ namespace ssh {
 
         private:
 
-            const Vector _arguments;
+            std::conditional_t<StoreRef, const Vector&, const Vector> _arguments;
             size_t _arg_index = 0;
             const size_t _max_index;
-            const Value _initial_value;
+            std::conditional_t<StoreRef, const Value&, const Value> _initial_value;
             Value _last_value;
-            const Function _function;
-            const Coeffs _coefficients;
+            std::conditional_t<StoreRef, const Function&, const Function> _function;
+            std::conditional_t<StoreRef, const Coeffs&, const Coeffs> _coefficients;
 
         };
 
-        template<typename Argument, typename Function, typename Coeffs, typename Value = Argument, typename KVector = types::vector1d_t<Value>>
+        template<
+            typename Argument, typename Function, typename Coeffs, 
+            bool StoreRef = false,
+            typename Value = Argument, 
+            typename KVector = types::vector1d_t<Value>>
         class lazy_runge_kutta_uniform {
 
         public:
@@ -108,15 +116,18 @@ namespace ssh {
             lazy_runge_kutta_uniform(const lazy_runge_kutta_uniform&) = default;
             lazy_runge_kutta_uniform(lazy_runge_kutta_uniform&&) = default;
 
-            lazy_runge_kutta_uniform(const Argument& a, const Argument& b, const size_t n, const Value initial_value,
-                                     const Function function, const Coeffs coefficients) :
+            lazy_runge_kutta_uniform(const Argument& a, const Argument& b, const size_t n, const Value& initial_value,
+                                     const Function& function, const Coeffs& coefficients) :
                     _d((b - a) / (n - 1)), _left_argument(a), _right_argument(b), 
                     _last_argument(a), _initial_value(initial_value), _last_value(initial_value),
                     _function(function), _coefficients(coefficients) {}
 
-            lazy_runge_kutta_uniform(const Argument& a, const Argument& b, const size_t n, const Value initial_value,
-                                     const Function function) :
-                    lazy_runge_kutta_uniform(a, b, n, initial_value, function, Coeffs()) {}
+            lazy_runge_kutta_uniform(const Argument& a, const Argument& b, const size_t n, const Value& initial_value,
+                                     const Function& function) :
+                    lazy_runge_kutta_uniform(a, b, n, initial_value, function, Coeffs()) 
+            {
+                static_assert(!StoreRef, "Coeffitients must be provided to store reference");
+            }
 
             Value operator()(KVector& k) {
                 if (!*this)
@@ -147,17 +158,19 @@ namespace ssh {
 
         private:
 
-            const Argument _d, _left_argument, _right_argument;
+            const Argument _d;
+            std::conditional_t<StoreRef, const Argument&, const Argument> _left_argument, _right_argument;
             Argument _last_argument;
-            const Value _initial_value;
+            std::conditional_t<StoreRef, const Value&, const Value> _initial_value;
             Value _last_value;
-            const Function _function;
-            const Coeffs _coefficients;
+            std::conditional_t<StoreRef, const Function&, const Function> _function;
+            std::conditional_t<StoreRef, const Coeffs&, const Coeffs> _coefficients;
 
         };
 
         template<
             typename AVector, typename VVector, typename Function, typename Coeffs, 
+            bool StoreRef = false,
             typename Argument = typename AVector::value_type, 
             typename Value = typename VVector::value_type,
             typename KVector = types::vector2d_t<Value>,
@@ -181,7 +194,10 @@ namespace ssh {
 
                 lazy_runge_kutta_p(const AVector& arguments, const VVector& initial_value, const Function& function,
                                    const PVector<std::tuple<Parameters...>>& parameters) :
-                    lazy_runge_kutta_p(arguments, initial_value, function, parameters, Coeffs()) {}
+                    lazy_runge_kutta_p(arguments, initial_value, function, parameters, Coeffs()) 
+                {
+                    static_assert(!StoreRef, "Coeffitients must be provided to store reference");
+                }
 
                 VVector operator()(KVector& k) {
                     if (!*this)
@@ -224,21 +240,24 @@ namespace ssh {
 
             private:
 
-                typedef caller<Argument, Function, Value, Parameters...> _caller_t;
+                typedef caller<Argument, Function, StoreRef, Value, Parameters...> _caller_t;
 
-                const AVector _arguments;
-                const PVector<std::tuple<Parameters...>> _parameters;
+                std::conditional_t<StoreRef, const AVector&, const AVector> _arguments;
+                std::conditional_t<StoreRef, 
+                    const PVector<std::tuple<Parameters...>>&, 
+                    const PVector<std::tuple<Parameters...>>> _parameters;
                 size_t _arg_index = 0;
                 const size_t _max_index;
-                const AVector _initial_value;
+                std::conditional_t<StoreRef, const AVector&, const AVector> _initial_value;
                 AVector _last_value;
                 const _caller_t _caller;
-                const Coeffs _coefficients;
+                std::conditional_t<StoreRef, const Coeffs&, const Coeffs> _coefficients;
 
         }; 
 
         template<
             typename Argument, typename Function, typename Coeffs, 
+            bool StoreRef = false,
             typename VVector = types::vector1d_t<Argument>, 
             typename Value = typename VVector::value_type,
             typename KVector = types::vector2d_t<Value>,
@@ -259,14 +278,17 @@ namespace ssh {
                                            const VVector& initial_value, const Function& function, 
                                            const PVector<std::tuple<Parameters...>>& parameters, 
                                            const Coeffs& coefficients) :
-                    _d((b - a) / Argument(n)), _left_argument(a), _right_argument(b), _last_argument(a), 
+                    _d((b - a) / Argument(n - 1)), _left_argument(a), _right_argument(b), _last_argument(a), 
                     _initial_value(initial_value), _last_value(initial_value), _parameters(parameters),
                     _caller(_caller_t(function)), _coefficients(coefficients) {}
 
                 lazy_runge_kutta_uniform_p(const Argument& a, const Argument& b, const size_t n, 
                                            const VVector& initial_value, const Function& function, 
                                            const PVector<std::tuple<Parameters...>>& parameters) :
-                    lazy_runge_kutta_uniform_p(a, b, n, initial_value, function, parameters, Coeffs()) {}
+                    lazy_runge_kutta_uniform_p(a, b, n, initial_value, function, parameters, Coeffs()) 
+                {
+                    static_assert(!StoreRef, "Coeffitients must be provided to store reference");
+                }
 
                 VVector operator()(KVector& k) {
                     if (!*this)
@@ -308,15 +330,18 @@ namespace ssh {
 
             private:
 
-                typedef caller<Argument, Function, Value, Parameters...> _caller_t;
+                typedef caller<Argument, Function, StoreRef, Value, Parameters...> _caller_t;
 
-                const Argument _d, _left_argument, _right_argument;
+                const Argument _d;
+                std::conditional_t<StoreRef, const Argument&, const Argument> _left_argument, _right_argument;
                 Argument _last_argument;
-                const VVector _initial_value;
+                std::conditional_t<StoreRef, const VVector&, const VVector> _initial_value;
                 VVector _last_value;
-                const PVector<std::tuple<Parameters...>> _parameters;
+                std::conditional_t<StoreRef, 
+                    const PVector<std::tuple<Parameters...>>&,
+                    const PVector<std::tuple<Parameters...>>> _parameters;
                 const _caller_t _caller;
-                const Coeffs _coefficients;
+                std::conditional_t<StoreRef, const Coeffs&, const Coeffs> _coefficients;
 
         };
         
@@ -331,15 +356,27 @@ namespace ssh {
             runge_kutta(const Coeffs& coefficients) : _coefficients(coefficients) {}
 
             template<typename Function, typename Vector = types::vector1d_t<Argument>, typename KVector = types::vector1d_t<Value>>
-            auto create_lazy(const Vector& arguments, const Value& initial_value, const Function& function) {
+            auto create_lazy(const Vector& arguments, const Value& initial_value, const Function& function) const {
                 return implementation::
-                    lazy_runge_kutta<Vector, Value, Function, Coeffs, KVector>(arguments, initial_value, function, _coefficients);
+                    lazy_runge_kutta<Vector, Value, Function, Coeffs, false, KVector>(arguments, initial_value, function, _coefficients);
             }
 
             template<typename Function, typename KVector = types::vector1d_t<Value>>
-            auto create_lazy_uniform(const Argument& a, const Argument& b, size_t n, const Value& initial_value, const Function& function) {
+            auto create_lazy_uniform(const Argument& a, const Argument& b, size_t n, const Value& initial_value, const Function& function) const {
                 return implementation::
-                    lazy_runge_kutta_uniform<Argument, Function, Coeffs, Value, KVector>(a, b, n, initial_value, function, _coefficients);
+                    lazy_runge_kutta_uniform<Argument, Function, Coeffs, false, Value, KVector>(a, b, n, initial_value, function, _coefficients);
+            }
+
+            template<typename Function, typename Vector = types::vector1d_t<Argument>, typename KVector = types::vector1d_t<Value>>
+            auto create_lazy_bound(const Vector& arguments, const Value& initial_value, const Function& function) const {
+                return implementation::
+                    lazy_runge_kutta<Vector, Value, Function, Coeffs, true, KVector>(arguments, initial_value, function, _coefficients);
+            }
+
+            template<typename Function, typename KVector = types::vector1d_t<Value>>
+            auto create_lazy_uniform_bound(const Argument& a, const Argument& b, size_t n, const Value& initial_value, const Function& function) const {
+                return implementation::
+                    lazy_runge_kutta_uniform<Argument, Function, Coeffs, true, Value, KVector>(a, b, n, initial_value, function, _coefficients);
             }
 
             template<
@@ -351,10 +388,26 @@ namespace ssh {
                 typename KSubVector = typename KVector::value_type,
                 typename... Parameters>
             auto create_lazy_p(const AVector& arguments, const VVector& initial_value, const Function& function,
-                               const PVector<std::tuple<Parameters...>>& parameters) 
+                               const PVector<std::tuple<Parameters...>>& parameters) const
             {
                 return implementation::
-                    lazy_runge_kutta_p<AVector, VVector, Function, Coeffs, Argument, Value, KVector, PVector, KSubVector, Parameters...>
+                    lazy_runge_kutta_p<AVector, VVector, Function, Coeffs, false, Argument, Value, KVector, PVector, KSubVector, Parameters...>
+                        (arguments, initial_value, function, parameters, _coefficients);
+            }
+
+            template<
+                typename Function,
+                typename KVector = types::vector2d_t<Value>,
+                typename AVector = types::vector1d_t<Argument>,
+                typename VVector = types::vector1d_t<Value>,
+                template<typename> typename PVector = types::vector1d_t, 
+                typename KSubVector = typename KVector::value_type,
+                typename... Parameters>
+            auto create_lazy_bound_p(const AVector& arguments, const VVector& initial_value, const Function& function,
+                               const PVector<std::tuple<Parameters...>>& parameters) const
+            {
+                return implementation::
+                    lazy_runge_kutta_p<AVector, VVector, Function, Coeffs, true, Argument, Value, KVector, PVector, KSubVector, Parameters...>
                         (arguments, initial_value, function, parameters, _coefficients);
             }
 
@@ -366,10 +419,25 @@ namespace ssh {
                 typename KSubVector = typename KVector::value_type,
                 typename... Parameters>
             auto create_lazy_uniform_p(const Argument& a, const Argument& b, const size_t n, const VVector& initial_value,
-                                       const Function& function, const PVector<std::tuple<Parameters...>>& parameters) 
+                                       const Function& function, const PVector<std::tuple<Parameters...>>& parameters) const
             {
                 return implementation::
-                    lazy_runge_kutta_uniform_p<Argument, Function, Coeffs, VVector, Value, KVector, PVector, KSubVector, Parameters...>
+                    lazy_runge_kutta_uniform_p<Argument, Function, Coeffs, false, VVector, Value, KVector, PVector, KSubVector, Parameters...>
+                        (a, b, n, initial_value, function, parameters, _coefficients); 
+            }
+
+            template<
+                typename Function,
+                typename KVector = types::vector2d_t<Value>,
+                typename VVector = types::vector1d_t<Argument>, 
+                template<typename> typename PVector = types::vector1d_t, 
+                typename KSubVector = typename KVector::value_type,
+                typename... Parameters>
+            auto create_lazy_uniform_bound_p(const Argument& a, const Argument& b, const size_t n, const VVector& initial_value,
+                                             const Function& function, const PVector<std::tuple<Parameters...>>& parameters) const
+            {
+                return implementation::
+                    lazy_runge_kutta_uniform_p<Argument, Function, Coeffs, true, VVector, Value, KVector, PVector, KSubVector, Parameters...>
                         (a, b, n, initial_value, function, parameters, _coefficients); 
             }
 
