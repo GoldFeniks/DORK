32e135aa370a283e58acb03219afb1a58612d28b
Tyshchenko Andrey <ggoldenfeniks@gmail.com>
2018-10-23 13:51:36 +1000

    [utils] Add constructor class

--- source/include/utils/utils.hpp
+++ source/include/utils/utils.hpp
@@ -3,6 +3,8 @@
 #include <cstddef>
 #include <functional>
 #include <vector>
+#include <type_traits>
+#include <tuple>
 
 namespace ssh {
 
@@ -55,6 +57,84 @@ namespace ssh {
         T factorial(const I n) {
             return n <= I(1) ? T(1) : factorial<T>(n - I(1)) * T(n);
         }
+
+        template<typename... Args>
+        struct arguments {
+
+            using args_t = std::tuple<Args...>;
+
+            const args_t args;
+            
+            arguments(const Args&... args) : args(std::tuple(args...)) {}
+
+        };
+
+        struct no_arguments {
+
+            using args_t = std::tuple<>;
+            
+            const args_t args;
+
+        };
+
+        template<typename T, typename Tuple, typename = typename Tuple::args_t>
+        struct is_constructible_from_tuple_types;
+
+        template<typename T, typename Tuple, typename... Args>
+        struct is_constructible_from_tuple_types<T, Tuple, std::tuple<Args...>> {
+
+            static constexpr bool value = std::is_constructible_v<T, Args...>;
+
+        };
+
+        template<typename T, typename Tuple>
+        inline constexpr bool is_constructible_from_tuple_types_v = is_constructible_from_tuple_types<T, Tuple>::value;
+
+        template<typename T>
+        class constructor {
+
+            public:
+
+                template<typename... Args>
+                static auto construct_from_tuple(const std::tuple<Args...>& args) {
+                    return choose_constructor<0, Args...>::type::construct(args);
+                }
+
+                template<typename... Args>
+                static auto construct(const Args... args) {
+                    return construct_from_tuple<Args...>(std::tuple<Args...>(args...));
+                }
+
+            private:
+
+                template<size_t N>
+                struct _constructor {
+
+                    template<typename... Args>
+                    static auto construct(const std::tuple<Args...>& args) {
+                        return std::apply([](auto&&... args) { 
+                            return T(std::forward<decltype(args)>(args)...); 
+                        }, std::get<N>(args).args);
+                    }
+
+                };
+
+                template<size_t N, typename TArgs, typename... Tail>
+                struct choose_constructor {
+
+                    using type = std::conditional_t<is_constructible_from_tuple_types_v<T, TArgs>,
+                                                    _constructor<N>, typename choose_constructor<N + 1, Tail...>::type>;
+
+                };
+
+                template<size_t N, typename TArgs>
+                struct choose_constructor<N, TArgs> {
+                    
+                    using type = std::enable_if_t<is_constructible_from_tuple_types_v<T, TArgs>, _constructor<N>>;
+
+                };
+
+        };
 
     }// namespace utils
 
